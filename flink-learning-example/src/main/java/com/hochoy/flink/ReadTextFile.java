f35eb70356b44ba59dc6dd79d0b672e72e4729c4
hochoy18 <hochoy18@sina.com>
2020-09-27 19:27:31 +0800

    'readTextFile'

--- flink-learning-example/src/main/java/com/hochoy/flink/ReadTextFile.java
+++ flink-learning-example/src/main/java/com/hochoy/flink/ReadTextFile.java
@@ -1,32 +1,58 @@
 package com.hochoy.flink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.GroupCombineFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.MapOperator;
+import org.apache.flink.api.java.operators.ReduceOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
 
 public class ReadTextFile {
-    public static void main(String[] args) {
+//    public ReadTextFile() {
+//    }
+
+    public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
         DataSet<String> dataSource = env.readTextFile(System.getProperty("user.dir") + "\\test_data\\test1\\WordCount.txt");
 
-        dataSource.flatMap(new FlatMapFunction<String, String>() {
+        FlatMapOperator<String, String> flatMap = dataSource.flatMap(new FlatMapFunction<String, String>() {
             @Override
             public void flatMap(String value, Collector<String> out) throws Exception {
-                Arrays.asList(value.split(" ")).stream().forEach(e -> out.collect(e));
+                Arrays.stream(value.split(" ")).forEach(out::collect);
             }
-        }).map(new MapFunction<String, Tuple2<String, Long>>() {
+        });
+
+        flatMap.print();
+        System.out.println("------------------------------");
+
+        MapOperator<String, Tuple2<String, Integer>> map = flatMap.map(new MapFunction<String, Tuple2<String, Integer>>() {
             @Override
-            public Tuple2<String, Long> map(String value) throws Exception {
-                return new Tuple2<String, Long>(value, 1L);
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                return   Tuple2.of(value, 1);
             }
-        }).groupBy(1);
+        });
+
+        map.print();
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+        ReduceOperator<Tuple2<String, Integer>> reduce = map.groupBy(1).reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> reduce(Tuple2<String, Integer> value1, Tuple2<String, Integer> value2) throws Exception {
+                return Tuple2.of(value1.f0, value1.f1 + value2.f1);
+            }
+        });
+        System.out.println("reduce========================");
+        reduce.print();
+
+
+
 
         dataSource
                 .flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
@@ -35,23 +61,29 @@ public class ReadTextFile {
                         Arrays.asList(value.split(" ")).stream().forEach(e -> out.collect(new Tuple2<>(e, 1l)));
                     }
                 });
+        try {
+            dataSource.print();
+//            env.execute("read text file batch ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
-        dataSource
-                .groupBy(0)
-                .combineGroup(new GroupCombineFunction<String, Tuple2<String, Long>>() {
-                    @Override
-                    public void combine(Iterable<String> values, Collector<Tuple2<String, Long>> out) throws Exception {
-                        String key = null;
-                        int count = 0;
-
-                        for (String word : values) {
-                            key = word;
-                            count++;
-                        }
-                        // emit tuple with word and count
-                        out.collect(new Tuple2(key, count));
-                    }
-                });
+//        dataSource
+//                .groupBy(0)
+//                .combineGroup(new GroupCombineFunction<String, Tuple2<String, Long>>() {
+//                    @Override
+//                    public void combine(Iterable<String> values, Collector<Tuple2<String, Long>> out) throws Exception {
+//                        String key = null;
+//                        int count = 0;
+//
+//                        for (String word : values) {
+//                            key = word;
+//                            count++;
+//                        }
+//                        // emit tuple with word and count
+//                        out.collect(new Tuple2(key, count));
+//                    }
+//                });
     }
 }
---
+++ flink-learning-example/src/main/java/com/hochoy/flink/streaming/KafkaSource.java
@@ -0,0 +1,37 @@
+package com.hochoy.flink.streaming;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+public class KafkaSource {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env =  StreamExecutionEnvironment.getExecutionEnvironment();
+
+        String topic  = "";
+        Properties props = new Properties();
+        props.put("bootstrap.servers","172.172.177.70:9092,172.172.177.72:9092,172.172.177.73:9092");
+//        props.put("zookeeper.connect", parameterTool.get("");
+        props.put("group.id","k_interface_call");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "latest");
+
+
+
+//
+//        FlinkKafkaConsumer010  consumer = new FlinkKafkaConsumer010<>(
+//                topic,
+//                new SimpleStringSchema(Charset.forName("UTF-8")),
+//                props);
+
+
+        env.execute("flink learning connectors kafka");
+    }
+
+}
