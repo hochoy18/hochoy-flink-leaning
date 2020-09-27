64199f0c6cbf725be229cca97ae515bd20a951b5
hochoy18 <hochoy18@sina.com>
2020-09-27 20:19:47 +0800

    flink-1.11

--- flink-learning-example/src/main/java/com/hochoy/flink/streaming/KafkaSource.java
+++ flink-learning-example/src/main/java/com/hochoy/flink/streaming/KafkaSource.java
@@ -4,7 +4,7 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 
 import java.nio.charset.Charset;
 import java.util.Properties;
