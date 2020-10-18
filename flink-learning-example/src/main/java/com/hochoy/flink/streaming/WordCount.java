efab31e359f5bee3329b609441911b57d1c9bcdf
hochoy18 <hochoy18@sina.com>
2020-10-18 16:39:48 +0800

    wordCount kafka

--- flink-learning-example/src/main/java/com/hochoy/flink/streaming/WordCount.java
+++ flink-learning-example/src/main/java/com/hochoy/flink/streaming/WordCount.java
@@ -1,18 +1,25 @@
 package com.hochoy.flink.streaming;
 
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
+import java.util.Properties;
 
 public class WordCount {
 
@@ -32,12 +39,57 @@ public class WordCount {
                 String host = tool.get("host");
                 int port = tool.getInt("port");
                 socket(host,port);
+            case "kafka":
+                /**
+                 * --type kafka --bootstrap.server localhost:9092  --topic part3-topic
+                 */
+                String bootstrapServer = tool.get("bootstrap.server");
+                String topic = tool.get("topic");
+                Properties props = new Properties();
+                props.setProperty("bootstrap.servers",bootstrapServer);
+                props.setProperty("group.id","kafka-demo");
+                kafka(topic,props);
+
         }
         streamEnv.execute(WordCount.class.getName() + "-" + type);
 
 
 
     }
+
+    private static void kafka(String topic, Properties properties){
+        DataStreamSource<String> source = streamEnv.addSource(new FlinkKafkaConsumer011<String>(
+                topic, new SimpleStringSchema(),
+                properties
+        ));
+        SingleOutputStreamOperator<JSONObject> map = source.map(new RichMapFunction<String, JSONObject>() {
+            @Override
+            public JSONObject map(String value) {
+                JSONObject res;
+                try {
+                    res = JSONObject.parseObject(value.trim());
+                } catch (JSONException e) {
+                    return null;
+                }
+                return res;
+            }
+        });
+        SingleOutputStreamOperator<Tuple3<String, String, Double>> scores = map.map(e -> {
+            String name = e.getString("name");
+            Double score = e.getDouble("score");
+            String subject = e.getString("subject");
+            return Tuple3.of(subject, name, score);
+        }).returns(Types.TUPLE(Types.STRING, Types.STRING, Types.DOUBLE));
+
+        // 某一个科目 所有学生的成绩
+        SingleOutputStreamOperator<Tuple3<String, String, Double>> sum = scores.keyBy(0).sum(2);
+        sum.print();
+
+        // 学生的总成绩
+        SingleOutputStreamOperator<Tuple3<String, String, Double>> sumIndividual = scores.keyBy(1).sum(2);
+        sumIndividual.print();
+    }
+
 
     private static void  socket(String host,int port){
 
---
+++ flink-learning-example/src/main/java/com/hochoy/kafka/KafkaProducerAnalysis.java
@@ -0,0 +1,207 @@
+package com.hochoy.kafka;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hochoy.utils.HochoyUtils;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class KafkaProducerAnalysis {
+
+
+    static int MSG_SIZE = (int)1e4;
+    static String TOPIC = "part3-topic";
+
+    public static void main(String[] args) {
+
+        Properties props = HochoyUtils.getProperties("producer.properties");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        String value ;
+
+        for (int i = 0 ; i < MSG_SIZE; i ++ ){
+
+//            ProducerRecord<String, String> record = new ProducerRecord<>( TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
+
+            value = genValue();
+            System.out.println(value);
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC,value);
+            Future<RecordMetadata> future = producer.send(record, (metadata, exception) -> {
+                if (exception == null) {
+                    System.out.println("Msg send success !!!! ");
+                    long offset = metadata.offset();
+                    int partition = metadata.partition();
+                    System.out.printf("partition : %d , offset : %d  %n", partition, offset);
+                } else {
+                    exception.printStackTrace();
+                }
+
+            });
+            RecordMetadata recordMetadata = null;
+            try {
+                recordMetadata = future.get();
+                long offset = recordMetadata.offset();
+                System.out.println("=========================="+ offset);
+//                if (i % 1000 == 0){
+                HochoyUtils.sleep(500);
+//                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+//        try {
+//            countDownLatch.await();
+//
+//        }catch (InterruptedException e ){
+//            e.printStackTrace();
+//        }
+
+
+//        int j = 0;
+//        while (j < 1e6){
+//            j++;
+//            int i = random.nextInt();
+//            ProducerRecord<String, String> record = new ProducerRecord<>(HochoyConstants.KafkaConstants.TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
+//            Future<RecordMetadata> future = producer.send(record, new Callback() {
+//                @Override
+//                public void onCompletion(RecordMetadata metadata, Exception exception) {
+//                    if (exception != null){
+//                        exception.printStackTrace();
+//                    }else {
+//                        System.out.printf("topic : %s%n partition : %s%n offset : %s%n key : %s%n value : %s%n------------------------------------------------",
+//                                metadata.topic() ,
+//                                metadata.partition() ,
+//                                metadata.offset(),
+//                                record.key(),
+//                                record.value());
+//                    }
+//                }
+//            });
+//            try {
+//                RecordMetadata metadata = future.get();
+//                String topic = metadata.topic();
+//                int partition = metadata.partition();
+//                long offset = metadata.offset();
+//                System.out.println(topic);
+//                System.out.println(partition);
+//                System.out.println(offset);
+//                System.out.println("--------------------");
+////                Thread.sleep(1000 / 100);
+//            }catch (InterruptedException| ExecutionException e ){
+//                logger.error("e:{}",e);
+//            }finally {
+//                producer.flush();
+//            }
+//
+//
+//        }
+
+    }
+    static Random random = new Random();
+    private static String genValue(){
+        JSONObject jo = new JSONObject();
+        jo.put("subject",getSubject());
+        jo.put("score",getScore());
+        jo.put("name",getFullName());
+        return jo.toJSONString();
+    }
+    private static String getSubject(){
+        String subjects[] = new String[]{
+                "Hadoop","Spark","Flink","Java","C++"
+        };
+        return subjects[random.nextInt(subjects.length)];
+    }
+
+    private static double getScore(){
+        double[] scores = new double[]{
+                90,91.5,99,98,93,89,88.5,89,100,79,78.5,98.5
+        };
+        return scores[random.nextInt(scores.length)];
+    }
+
+    private static String getFullName() {
+
+        String[] dan = new String[]{"李",
+                "王",
+                "张",
+                "刘",
+                "陈",
+                "杨",
+                "赵",
+                "黄",
+                "周",
+                "吴",
+                "徐",
+                "孙",
+                "胡",
+                "朱",
+                "高",
+                "林",
+                "何",
+                "郭",
+                "马",
+                "罗"
+        };
+        String[] fu = new String[]{"司马",
+                "上官",
+                "欧阳",
+                "夏侯",
+                "诸葛",
+                "闻人",
+                "南宫",
+                "西门",
+                "东门",
+                "左丘",
+                "梁丘"};
+
+        String[] ming = new String[]{"凯瑞",
+                "健雄",
+                "耀杰",
+                "潇然",
+                "子涵",
+                "越彬",
+                "钰轩",
+                "智辉",
+                "致远",
+                "俊驰",
+                "雨泽",
+                "烨磊",
+                "晟睿",
+                "文昊",
+                "修洁",
+                "黎昕",
+                "向澜",
+                "宇萧",
+                "星皓",
+                "潇咏",
+                "风明",
+                "成林",
+                "崇宁",
+                "思元",
+                "云浩",
+                "宇鸿"};
+
+
+        int i = random.nextInt(2);
+        int xingIndex = random.nextInt(3);
+        int mingIndex = random.nextInt(4);
+        String fullName = (i == 0 ? dan[xingIndex] : fu[xingIndex]) + ming[mingIndex];
+        return fullName;
+    }
+
+
+}
+
+
+
