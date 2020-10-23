774d8618e98fe816e28ba94b3536736d15dacf55
hochoy18 <hochoy18@sina.com>
2020-10-23 09:55:30 +0800

    kafka produce

--- flink-learning-example/src/main/java/com/hochoy/kafka/KafkaProducerAnalysis.java
+++ flink-learning-example/src/main/java/com/hochoy/kafka/KafkaProducerAnalysis.java
@@ -1,11 +1,16 @@
 package com.hochoy.kafka;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.hochoy.flink.entities.Metric;
 import com.hochoy.utils.HochoyUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -14,24 +19,24 @@ import java.util.concurrent.Future;
 public class KafkaProducerAnalysis {
 
 
-    private static int MSG_SIZE = (int)1e4;
-    private static String TOPIC = "part3-topic";
+    private final static int MSG_SIZE = (int) 1e4;
+    private static Random random = new Random();
+    private static DecimalFormat df = new DecimalFormat("#.00");
+    private static Properties props = HochoyUtils.getProperties("producer.properties");
+
+    private static KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
     public static void main(String[] args) {
 
-        Properties props = HochoyUtils.getProperties("producer.properties");
-
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-        String value ;
-
-        for (int i = 0 ; i < MSG_SIZE; i ++ ){
-
-//            ProducerRecord<String, String> record = new ProducerRecord<>( TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
-
-            value = genValue();
+        // args type
+        // metric ,  score
+        String type = args[0];
+        String value;
+        for (int i = 0; i < MSG_SIZE; i++) {
+            value = genValue(type);
             System.out.println(value);
-            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC,value);
+            String TOPIC = "part3-topic";
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, value);
             Future<RecordMetadata> future = producer.send(record, (metadata, exception) -> {
                 if (exception == null) {
                     System.out.println("Msg send success !!!! ");
@@ -43,161 +48,75 @@ public class KafkaProducerAnalysis {
                 }
 
             });
-            RecordMetadata recordMetadata = null;
+            RecordMetadata recordMetadata;
             try {
                 recordMetadata = future.get();
                 long offset = recordMetadata.offset();
-                System.out.println("=========================="+ offset);
-//                if (i % 1000 == 0){
+                System.out.println("==========================" + offset);
                 HochoyUtils.sleep(500);
-//                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
+            } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
-
-
-
         }
-//        try {
-//            countDownLatch.await();
-//
-//        }catch (InterruptedException e ){
-//            e.printStackTrace();
-//        }
-
-
-//        int j = 0;
-//        while (j < 1e6){
-//            j++;
-//            int i = random.nextInt();
-//            ProducerRecord<String, String> record = new ProducerRecord<>(HochoyConstants.KafkaConstants.TOPIC, random.nextInt(3), i+"", "{\"data\":[{\"country\":\"局域网\",\"request\":\"POST /action HTTP/1.1\",\"productid\":\"1\",\"city\":\"\",\"latitude\":\"31.1716369000\",\"ip\":\"192.168.1.243\",\"useragent\":\"Python-urllib/2.7\",\"servertime\":\"2020-03-04 18:30:03.000\",\"sessionid\":\"57c3b7ac4e5f4b22b710b3d06ee9faff\",\"lib_version\":\"1.0.0\",\"userid\":\"20000178769\",\"deviceid\":\"iOS_DEVICEID_4362806746292507\",\"uuid\":\"e6bc377f84c450767e2730e0a70653f1\",\"actionattach\":{},\"action\":\"$exitPage\",\"appkey\":\"23924869c35a4108b2539412fdf9c6f8\",\"clienttime\":\"2020-03-03 18:30:03.971\",\"region\":\"局域网\",\"properties\":{\"screen_width\":136,\"utm_campaign\":\"国庆节大促\",\"utm_medium\":\"TopBanner\",\"language\":\"lt_LT\",\"version\":\"2.1\",\"platform\":\"ios\",\"network\":\"WIFI\",\"manufacturer\":\"apple\",\"duration\":80087,\"screen_height\":1024,\"mccmnc\":\"46003\",\"is_new_device\":\"false\",\"refer\":\"\",\"is_update\":\"false\",\"model\":\"iPad4,1\",\"pagetitle\":\"PageTitle_897156978712\",\"page\":\"RegistviewController\",\"osversion\":\"3.1.2\",\"channelid\":\"baidu\",\"utm_content\":\"0815-method\",\"utm_source\":\"百度\"},\"longitude\":\"121.3368797000\"}]}");
-//            Future<RecordMetadata> future = producer.send(record, new Callback() {
-//                @Override
-//                public void onCompletion(RecordMetadata metadata, Exception exception) {
-//                    if (exception != null){
-//                        exception.printStackTrace();
-//                    }else {
-//                        System.out.printf("topic : %s%n partition : %s%n offset : %s%n key : %s%n value : %s%n------------------------------------------------",
-//                                metadata.topic() ,
-//                                metadata.partition() ,
-//                                metadata.offset(),
-//                                record.key(),
-//                                record.value());
-//                    }
-//                }
-//            });
-//            try {
-//                RecordMetadata metadata = future.get();
-//                String topic = metadata.topic();
-//                int partition = metadata.partition();
-//                long offset = metadata.offset();
-//                System.out.println(topic);
-//                System.out.println(partition);
-//                System.out.println(offset);
-//                System.out.println("--------------------");
-////                Thread.sleep(1000 / 100);
-//            }catch (InterruptedException| ExecutionException e ){
-//                logger.error("e:{}",e);
-//            }finally {
-//                producer.flush();
-//            }
-//
-//
-//        }
-
     }
-    static Random random = new Random();
-    private static String genValue(){
+
+
+    private static String genScore() {
+        String[] dan = new String[]{"李", "王", "张", "刘", "陈", "杨", "赵", "黄", "周", "吴", "徐", "孙", "胡", "朱", "高", "林", "何", "郭", "马", "罗"};
+        String[] fu = new String[]{"司马", "上官", "欧阳", "夏侯", "诸葛", "闻人", "南宫", "西门", "东门", "左丘", "梁丘"};
+        String[] ming = new String[]{"凯瑞", "健雄", "耀杰", "潇然", "子涵", "越彬", "钰轩", "智辉", "致远", "俊驰", "雨泽", "烨磊", "晟睿", "文昊", "修洁", "黎昕", "向澜", "宇萧", "星皓", "潇咏", "风明", "成林", "崇宁", "思元", "云浩", "宇鸿"};
+        String[] subjects = new String[]{"Hadoop", "Spark", "Flink", "Java", "C++"};
+        double[] scores = new double[]{90, 91.5, 99, 98, 93, 89, 88.5, 89, 100, 79, 78.5, 98.5};
         JSONObject jo = new JSONObject();
-        jo.put("subject",getSubject());
-        jo.put("score",getScore());
-        jo.put("name",getFullName());
-        return jo.toJSONString();
-    }
-    private static String getSubject(){
-        String subjects[] = new String[]{
-                "Hadoop","Spark","Flink","Java","C++"
-        };
-        return subjects[random.nextInt(subjects.length)];
-    }
-
-    private static double getScore(){
-        double[] scores = new double[]{
-                90,91.5,99,98,93,89,88.5,89,100,79,78.5,98.5
-        };
-        return scores[random.nextInt(scores.length)];
-    }
-
-    private static String getFullName() {
-
-        String[] dan = new String[]{"李",
-                "王",
-                "张",
-                "刘",
-                "陈",
-                "杨",
-                "赵",
-                "黄",
-                "周",
-                "吴",
-                "徐",
-                "孙",
-                "胡",
-                "朱",
-                "高",
-                "林",
-                "何",
-                "郭",
-                "马",
-                "罗"
-        };
-        String[] fu = new String[]{"司马",
-                "上官",
-                "欧阳",
-                "夏侯",
-                "诸葛",
-                "闻人",
-                "南宫",
-                "西门",
-                "东门",
-                "左丘",
-                "梁丘"};
-
-        String[] ming = new String[]{"凯瑞",
-                "健雄",
-                "耀杰",
-                "潇然",
-                "子涵",
-                "越彬",
-                "钰轩",
-                "智辉",
-                "致远",
-                "俊驰",
-                "雨泽",
-                "烨磊",
-                "晟睿",
-                "文昊",
-                "修洁",
-                "黎昕",
-                "向澜",
-                "宇萧",
-                "星皓",
-                "潇咏",
-                "风明",
-                "成林",
-                "崇宁",
-                "思元",
-                "云浩",
-                "宇鸿"};
-
-
-        int i = random.nextInt(2);
+        jo.put("subject", subjects[random.nextInt(subjects.length)]);
+        jo.put("score", scores[random.nextInt(scores.length)]);
         int xingIndex = random.nextInt(3);
         int mingIndex = random.nextInt(4);
-        String fullName = (i == 0 ? dan[xingIndex] : fu[xingIndex]) + ming[mingIndex];
-        return fullName;
+
+        jo.put("name", (random.nextInt(2) == 0 ? dan[xingIndex] : fu[xingIndex]) + ming[mingIndex]);
+        return jo.toJSONString();
+    }
+
+    private static String genMetric() {
+        Metric metric = new Metric();
+        metric.setTimestamp(System.currentTimeMillis());
+        metric.setName("mem");
+        Map<String, String> tags = new HashMap<>();
+        Map<String, Object> fields = new HashMap<>();
+        String[] ips = {"208.111.222.28", "97.175.198.251", "209.253.185.97", "84.65.84.130", "202.20.214.178", "205.239.31.228", "129.236.93.53", "38.217.124.159", "223.115.121.69", "125.10.193.197", "203.10.163.150", "168.217.61.32", "116.92.6.86", "227.178.88.92", "189.14.226.88", "11.31.143.8", "163.255.110.109", "109.20.130.97", "74.119.246.65", "153.200.13.43", "34.20.129.34", "178.218.127.152", "210.98.240.245", "117.128.254.55"};
+
+        int ipLen = ips.length;
+
+        tags.put("cluster", "hcohoy");
+        tags.put("host_ip", ips[random.nextInt(ipLen)]);
+
+        df = new DecimalFormat("#.00");
+        double percent = random.nextDouble();
+
+        fields.put("used_percent", df.format(percent * 100) + "%");
+        fields.put("max", df.format(random.nextDouble() * 100000000));
+        fields.put("used", df.format(random.nextDouble() * 1000000));
+        fields.put("init", df.format(random.nextDouble() * 100000));
+
+        metric.setTags(tags);
+        metric.setFields(fields);
+        return JSON.toJSONString(metric);
+    }
+
+    private static String genValue(String type) {
+        String res;
+        switch (type) {
+            case "score":
+                res = genScore();
+                break;
+            case "metric":
+                res = genMetric();
+                break;
+            default:
+                res = genMetric();
+                break;
+        }
+        return res;
     }
 
 
