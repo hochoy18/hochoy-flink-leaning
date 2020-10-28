c63f3185d210d120ef2a9c231102f1793e792284
hochoy18 <hochoy18@sina.com>
2020-10-28 17:46:02 +0800

    sink kafka and jdbc

--- flink-learning-example/src/main/scala/com/hochoy/flink/sink/JdbcSink.scala
+++ flink-learning-example/src/main/scala/com/hochoy/flink/sink/JdbcSink.scala
@@ -2,30 +2,89 @@ package com.hochoy.flink.sink
 
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
-import java.sql.{Connection, PreparedStatement}
-import java.sql.DriverManager
+import java.sql.{Connection, DriverManager, PreparedStatement, SQLException}
 
-class JdbcSink[Any] extends RichSinkFunction {
+import com.alibaba.fastjson.{JSON, JSONObject}
+import com.hochoy.utils.HochoyUtils
+
+class JdbcSink[String] extends RichSinkFunction[JSONObject] {
 
 
-  val ps: PreparedStatement = null
-  var connection: Connection = null
+  var ps: PreparedStatement = _
+  var connection: Connection = _
 
-  override def invoke(value: Nothing, context: SinkFunction.Context[_]): Unit = {
-    super.invoke(value, context)
-    var con = null
-//    try {
-//      Class.forName("com.mysql.jdbc.Driver")
-//      con = DriverManager getConnection("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8", "root", "root123456")
-//      DriverManager.getConnection("")
-//    } catch {
-//      case e: Exception =>
-//        System.out.println("-----------mysql get connection has exception , msg = " + e.getMessage)
-//    }
+  override  def invoke(value: JSONObject, context: SinkFunction.Context[_]): Unit = {
+
+
+   if (value != null){
+     ps.setObject(1,value getDate  "start_time")
+     ps.setObject(2,value getString "result_code")
+     ps.setObject(3,value getString "type")
+     ps.setObject(4, value getString "sign")
+     ps.setObject(5, value getInteger  "result_code")
+     ps.setObject(6, value getString "type")
+
+     ps.execute()
+   }
 
   }
 
-  override def open(parameters: Configuration): Unit = super.open(parameters)
+  override def open(parameters: Configuration): Unit = {
+    super.open(parameters)
+    val props = HochoyUtils.getProperties("jdbc.properties")
+    try {
+      Class.forName(props.getProperty("driver_class"))
+      connection = DriverManager.getConnection(props.getProperty("jdbc_url"), props.getProperty("jdbc_user"), props.getProperty("jdbc_pwd"))
+    } catch {
+      case e: SQLException => e.printStackTrace()
+    }
+    ps = connection.prepareStatement(
+      """
+        |INSERT INTO gdm_fact_user_login_de ( k_dt, store_code, staff_id, cust_source, fuid, staff_name)
+        |VALUES (?,?,?, ?,?,?)
+      """.stripMargin)
+  }
 
-  override def close(): Unit = super.close()
+  override def close(): Unit = {
+    super.close()
+    if (ps != null) {
+      ps.close()
+    }
+    if (connection != null) {
+      connection.close()
+    }
+  }
+
+
+}
+object JdbcSink {
+  def main(args: Array[String]): Unit = {
+    val props = HochoyUtils.getProperties("jdbc.properties")
+
+
+    val connection: Connection =    DriverManager.getConnection(props.getProperty("jdbc_url"), props.getProperty("jdbc_user"), props.getProperty("jdbc_pwd"))
+    val ps: PreparedStatement =   connection.prepareStatement(
+      """
+        |INSERT INTO gdm_fact_user_login_de ( k_dt, store_code, staff_id, cust_source, fuid, staff_name)
+        |VALUES (?,?,?, ?,?,?)
+      """.stripMargin)
+    try {
+      Class.forName(props.getProperty("driver_class"))
+
+
+      ps.setObject(1,"2020-10-28")
+      ps.setObject(2, "111")
+      ps.setObject(3,"19185407")
+      ps.setObject(4, "8002")
+      ps.setInt(5, 1)
+      ps.setObject(6, "cai.he")
+
+      ps.execute()
+      ps.close()
+      connection.close()
+    } catch {
+      case e: SQLException => e.printStackTrace()
+    }
+
+  }
 }
--- flink-learning-example/src/main/scala/com/hochoy/flink/streaming/MetricStream.scala
+++ flink-learning-example/src/main/scala/com/hochoy/flink/streaming/MetricStream.scala
@@ -2,11 +2,14 @@ package com.hochoy.flink.streaming
 
 import java.util.Properties
 
+import com.alibaba.fastjson.{JSON, JSONObject}
+import com.hochoy.flink.sink.JdbcSink
+import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.streaming.api.datastream.DataStreamSource
+import org.apache.flink.streaming.api.datastream.{DataStream, DataStreamSource, SingleOutputStreamOperator}
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
 
 object MetricStream {
 
@@ -23,6 +26,19 @@ object MetricStream {
     val source: DataStreamSource[String] = env.addSource(new FlinkKafkaConsumer011[String](sourceTopic,new SimpleStringSchema,props))
     println(source.getParallelism)
     source.print()
+
+    val value: DataStream[JSONObject] = source.map(new RichMapFunction[String, JSONObject] {
+      override def map(value: String): JSONObject = {
+        JSON.parseObject(value)
+      }
+    })
+
+    value.addSink(new JdbcSink[JSONObject])
+    val sink2: DataStream[String] = value.map(new RichMapFunction[JSONObject, String] {
+      override def map(value: JSONObject): String = value.toJSONString
+    })
+    sink2.addSink(new FlinkKafkaProducer011[String](bootstrapServer, sourceTopic,new SimpleStringSchema))
+
     env.execute(this.getClass.getName)
   }
 }
