7f8549537e237846639d0c6ff057dc88544d216a
hochoy18 <hochoy18@sina.com>
2020-10-29 17:19:13 +0800

    mvn.sh

--- mvn.sh
+++ mvn.sh
@@ -3,4 +3,6 @@ maven的mvn dependency依赖分析和常用命令介绍
 https://www.jianshu.com/p/516d8a5368e5
 
 mvn dependency:sources & mvn dependency:resolve -Dclassifier=javadoc
+
+git config http.sslVerify false
 # mvn dependency:tree >> tree.txt
